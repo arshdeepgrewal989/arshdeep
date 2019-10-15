e98bc6021d8cc40583004a19f582c16ed574fb50
Arshdeep <arshdeepgrewal989@gmail.com>
2019-10-15 02:23:22 -0400

    first edit. v.1.0

---
+++ src/resumeData.js
@@ -0,0 +1,95 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Arshdeep Grewal",
+    "role": "Full Stack Developer",
+    "linkedinId":"https://www.linkedin.com/in/arshdeep-grewal-495ab490/",
+    "roleDescription": "I enjoy working with Databases and Full stack development..",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/arshdeep-grewal-495ab490/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/arshdeepgrewal989",
+          "className":"fa fa-github"
+        },
+      ],
+    "aboutme":"Thank-you for visiting my website. I am a Computer Science student, currently enrolled in my 3rd year of study at Wilfrid Laurier University. This is my website which I created with the help of React. Here you can see my projects, experience, education, and contact information.",
+    "address":"Canada",
+    "website":"https://arshdeep.github.io",
+    "education":[
+      {
+        "UniversityName":"Wilfrid Laurier University",
+        "specialization":"Computer Science",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2022",
+        "Achievements":"Passed my classes"
+      },
+      
+    ],
+    "work":[
+      {
+        "CompanyName":"Wilfrid Lauirer University",
+        "specialization":"Student Office Admin Assistan",
+        "MonthOfLeaving":"Sep",
+        "YearOfLeaving":"2019",
+        "Achievements":"Helped create on campus navigation system. Worked with Archibus to log and manage space data through out campus. Also worked with AutoCAD to make and edit floor plans"
+      },
+      {
+        "CompanyName":"Results Fitness",
+        "specialization":"Swimming Instructor/ Lifeguard",
+        "MonthOfLeaving":"June",
+        "YearOfLeaving":"2019",
+        "Achievements":"Lead Instructor"
+      }
+    ],
+    "skillsDescription":"What Can I Do?",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"MapWize",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/wayfind1.png",
+        "imgurl":"images/portfolio/wayfind2.png"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"Sikkest Yute Ever",
+        "name":"Steve Jobs"
+      },
+      {
+        "description":"Pleasure to work with",
+        "name":"Elon Musk"
+      }
+    ]
+  }
+  
+  export default resumeData
