5191b0bfd312b3b7e7687c5f2b55488b9c476dca
Arshdeep <arshdeepgrewal989@gmail.com>
2019-10-15 02:58:00 -0400

    n

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,15 +7,15 @@ export default class Header extends Component {
       
       <header id="home">
          <nav id="nav-wrap">
-            <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
-          <a className="mobile-btn" href="#" title="Hide navigation">Hide navigation</a>
+            <a className="mobile-btn" href="/#nav-wrap" title="Show navigation">Show navigation</a>
+          <a className="mobile-btn" href="/#" title="Hide navigation">Hide navigation</a>
             <ul id="nav" className="nav">
-               <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
-               <li><a className="smoothscroll" href="#about">About</a></li>
-             <li><a className="smoothscroll" href="#resume">Resume</a></li>
-               <li><a className="smoothscroll" href="#portfolio">Works</a></li>
-               <li><a className="smoothscroll" href="#testimonials">Testimonials</a></li>
-               <li><a className="smoothscroll" href="#contact">Contact</a></li>
+               <li className="current"><a className="smoothscroll" href="/#home">Home</a></li>
+               <li><a className="smoothscroll" href="/#about">About</a></li>
+             <li><a className="smoothscroll" href="/#resume">Resume</a></li>
+               <li><a className="smoothscroll" href="/#portfolio">Works</a></li>
+               <li><a className="smoothscroll" href="/#testimonials">Refrences</a></li>
+               <li><a className="smoothscroll" href="/#contact">Contact</a></li>
             </ul>
          </nav>
 
@@ -41,7 +41,7 @@ export default class Header extends Component {
          </div>
 
          <p className="scrolldown">
-            <a className="smoothscroll" href="#about"><i className="icon-down-circle"></i></a>
+            <a className="smoothscroll" href="/#about"><i className="icon-down-circle"></i></a>
          </p>
 
       </header>
